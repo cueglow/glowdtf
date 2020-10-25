8dd7b1add7a8495a6ce084aca19756c17fbd4c5f
Steffen Burkowitz <mail@doriansnowball.de>
2020-10-25 20:21:55 +0100

    Add gradle run; Update gradlew to 6.7

--- cueglow-server/build.gradle.kts
+++ cueglow-server/build.gradle.kts
@@ -2,10 +2,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.10"
+    application
 }
 
 group = "org.cueglow"
 version = "0.0.1-dev"
+
+application {
+    mainClass.set("org.cueglow.server.MainKt")
+}
 
 repositories {
     mavenCentral()
@@ -13,7 +18,17 @@ repositories {
 dependencies {
     testImplementation(kotlin("test-junit5"))
     implementation("io.javalin:javalin:3.11.2")
+    implementation("org.apache.logging.log4j:log4j-api:2.13.3")
+    implementation("org.apache.logging.log4j:log4j-core:2.13.3")
+    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.13.3")
 }
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "13"
 }
+
+// Copy the static files from webui into the jar
+//processResources {
+//    from ('frontend/dist/') {
+//        into 'public'
+//    }
+//}
--- cueglow-server/src/main/kotlin/org/cueglow/server/Main.kt
+++ cueglow-server/src/main/kotlin/org/cueglow/server/Main.kt
@@ -2,7 +2,9 @@ package org.cueglow.server
 
 import io.javalin.Javalin
 
+
 fun main(args: Array<String>) {
+    println("Starting Server")
     val app = Javalin.create().start(7000)
     app.get("/") { ctx -> ctx.result("Hello World") }
 }
