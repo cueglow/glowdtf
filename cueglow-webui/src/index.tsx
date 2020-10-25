b6300eafc725d554316ca28e2f7413167fc53236
Johannes <jcmf.schule@gmail.com>
2020-10-25 23:43:46 +0100

    Add test page with reconnecting websockets

--- cueglow-webui/src/index.tsx
+++ cueglow-webui/src/index.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReconnectingWebSocket from 'reconnecting-websocket';
+
+console.log("starting...")
+
+// TODO
+// serve this via Kotlin server
+
+// TODO remove hardcoded websocket url
+// make websocket request to url from which website was served
+let ws = new ReconnectingWebSocket("ws://localhost:7000/ws", [],
+  { maxReconnectionDelay: 2000, minReconnectionDelay: 1000, debug: true});
+
+ws.send("testing, testing");
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +23,19 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  let [readyState, setReadyState] = useState(ws.readyState);
+  ws.addEventListener(
+    "close", (e) => { setTimeout(() => setReadyState(ws.readyState), 50) }
+  );
+  ws.addEventListener("open", e => (setReadyState(ws.readyState)));
+
+  return (
+    <div className="App">
+      {readyState}
+    </div>
+  );
+}
+
+
+
