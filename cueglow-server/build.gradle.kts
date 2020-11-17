cb39748973d9c57ad6cbee0bb7582180459b97d1
Johannes <32422182+Firionus@users.noreply.github.com>
2020-11-17 23:52:21 +0100

    Serve WebUI (#12)
    * npm build and copy into jar added to gradle
    
    Also serves static files from this directory now.
    
    Do `gradlew run` and navigate to localhost:7000 to
    see it serving the React App.
    
    Still waiting for client-side-navigation React app
    to be merged into main to test with client side nav.
    
    * Adjust routing for client-side navigation
    
    Uses `addSinglePageRoot` for Javalin backend and
    `default` prop on Reach Router React frontend.
    
    Routing of the webui seems to work now.
    
    * Remove superfluous comments
    
    * Add standard .gitignore in cueglow-server
    
    * Change root .gitignore to only IntelliJ

--- cueglow-server/build.gradle.kts
+++ cueglow-server/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.4.10"
     application
+    id("com.github.node-gradle.node") version "2.2.4"
 }
 
 group = "org.cueglow"
@@ -31,9 +32,6 @@ tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "13"
 }
 
-// Copy the static files from webui into the jar
-//processResources {
-//    from ('frontend/dist/') {
-//        into 'public'
-//    }
-//}
+
+apply(from="npm.gradle")
+
