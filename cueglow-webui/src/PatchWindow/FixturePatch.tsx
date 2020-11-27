bdc7df1a355532fd2de87c1ac3376e04bd53bdfb
Johannes <jcmf.schule@gmail.com>
2020-11-28 00:30:02 +0100

    Tabulator Example

--- cueglow-webui/src/PatchWindow/FixturePatch.tsx
+++ cueglow-webui/src/PatchWindow/FixturePatch.tsx
@@ -1,7 +1,10 @@
 
-import React, { useMemo, useState } from "react";
+import React from "react";
 import { Button } from "@blueprintjs/core";
-import { useTable } from "react-table";
+
+import 'react-tabulator/lib/styles.css'; // required styles
+import 'react-tabulator/lib/css/tabulator_midnight.min.css'; // theme
+import { ReactTabulator } from 'react-tabulator'
 
 // Import SASS-variables from blueprint.js
 /* eslint import/no-webpack-loader-syntax: off */
@@ -9,42 +12,41 @@ const bp = require('sass-extract-loader!@blueprintjs/core/lib/scss/variables.scs
 export function FixturePatch() {
 
     const columns = [
-        { id: "fid", accessor: (row: { fid: number; }) => row.fid, Header: "FID" },
-        { id: "name", accessor: (row: { name: string; }) => row.name, Header: "Name" },
-        { id: "fixtureType", accessor: (row: { fixtureType: string; }) => row.fixtureType, Header: "Fixture Type" },
-        { id: "dmxMode", accessor: (row: { dmxMode: string; }) => row.dmxMode, Header: "DMX Mode" },
-        { id: "universe", accessor: (row: { universe: number; }) => row.universe, Header: "Universe" },
-        { id: "address", accessor: (row: { address: number; }) => row.address, Header: "Address" },
+        { field: "fid", title: "FID" },
+        { field: "name", title: "Name" },
+        { field: "fixtureType", title: "Fixture Type" },
+        { field: "dmxMode", title: "DMX Mode" },
+        { field: "universe", title: "Universe" },
+        { field: "address", title: "Address" },
     ];
-    const data = useMemo(
-        () => [
-            {
-                fid: 1,
-                name: "Lamp 1",
-                fixtureType: "GLP impression Spot One",
-                dmxMode: "Standard (21 ch)",
-                universe: 1,
-                address: 1,
-            },
-            {
-                fid: 2,
-                name: "Lamp 2",
-                fixtureType: "GLP impression Spot One",
-                dmxMode: "Standard (21 ch)",
-                universe: 1,
-                address: 22,
-            },
-        ],
-        []
-    );
-    const tableInstance = useTable({ columns: columns, data: data });
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow
-    } = tableInstance;
+    const data = [
+        {
+            fid: 1,
+            name: "Lamp 1",
+            fixtureType: "GLP impression Spot One",
+            dmxMode: "Standard (21 ch)",
+            universe: 1,
+            address: 1,
+        },
+        {
+            fid: 2,
+            name: "Lamp 2",
+            fixtureType: "GLP impression Spot One",
+            dmxMode: "Standard (21 ch)",
+            universe: 1,
+            address: 22,
+        },
+        {
+            fid: 10,
+            name: "Front",
+            fixtureType: "Dimmer",
+            dmxMode: "8-bit (1 ch)",
+            universe: 1,
+            address: 43,
+        },
+    ];
+
+
     return (
         <div style={{
             position: "absolute",
@@ -65,47 +67,10 @@ export function FixturePatch() {
             <div style={{
                 flexGrow: 1,
             }}>
-                <table {...getTableProps()}>
-                    <thead>
-                        {// Loop over the header rows
-                            headerGroups.map(headerGroup => (
-                                // Apply the header row props
-                                <tr {...headerGroup.getHeaderGroupProps()}>
-                                    {// Loop over the headers in each row
-                                        headerGroup.headers.map(column => (
-                                            // Apply the header cell props
-                                            <th {...column.getHeaderProps()}>
-                                                {// Render the header
-                                                    column.render('Header')}
-                                            </th>
-                                        ))}
-                                </tr>
-                            ))}
-                    </thead>
-                    {/* Apply the table body props */}
-                    <tbody {...getTableBodyProps()}>
-                        {// Loop over the table rows
-                            rows.map(row => {
-                                // Prepare the row for display
-                                prepareRow(row)
-                                return (
-                                    // Apply the row props
-                                    <tr {...row.getRowProps()}>
-                                        {// Loop over the rows cells
-                                            row.cells.map(cell => {
-                                                // Apply the cell props
-                                                return (
-                                                    <td {...cell.getCellProps()}>
-                                                        {// Render the cell contents
-                                                            cell.render('Cell')}
-                                                    </td>
-                                                )
-                                            })}
-                                    </tr>
-                                )
-                            })}
-                    </tbody>
-                </table>
+                <ReactTabulator
+                    data={data}
+                    columns={columns}
+                />
             </div>
         </div>
     );
