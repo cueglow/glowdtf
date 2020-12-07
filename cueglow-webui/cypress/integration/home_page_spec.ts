a7d218afd1b09173d46126a1212ba0fd21ee4887
Johannes <jcmf.schule@gmail.com>
2020-12-07 01:26:04 +0100

    Add Cypress Navigation Test and disable examples

---
+++ cueglow-webui/cypress/integration/home_page_spec.ts
@@ -0,0 +1,40 @@
+function pathEquals(x) {
+    return cy.location("pathname").should("equal", x)
+}
+
+describe("Navigation", () => {
+    it("navigates through specified URLs when clicking Buttons", () => {
+        cy.visit("/")
+
+        cy.contains("Patch").click()
+        pathEquals("/patch")
+
+        // check that right tab is selected, even after reload
+        cy.contains("Fixtures").should("have.attr", "aria-selected", "true")
+        cy.contains("Fixture Types").should("have.attr", "aria-selected", "false")
+        cy.reload()
+        cy.contains("Fixtures").should("have.attr", "aria-selected", "true")
+
+        cy.contains("Fixture Types").click()
+        pathEquals("/patch/fixtureTypes")
+
+        cy.contains("Fixture Types").should("have.attr", "aria-selected", "true")
+        cy.reload()
+        cy.contains("Fixture Types").should("have.attr", "aria-selected", "true")
+
+        cy.contains("Fixtures").click()
+        cy.contains("Fixtures").should("have.attr", "aria-selected", "true")
+        pathEquals("/patch")
+
+        cy.contains("Add New Fixture").click()
+        pathEquals("/patch/newFixture")
+
+        cy.contains("Esc").click()
+        pathEquals("/patch")
+        
+        cy.contains("Esc").click()
+        pathEquals("/")
+    })
+
+    // TODO test keyboard navigation
+})
