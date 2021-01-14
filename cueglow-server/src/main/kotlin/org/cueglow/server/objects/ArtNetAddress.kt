e60d0de346b2350490ecfa33006d9f6d24d8a3e6
Johannes Fried <jcmf.schule@gmail.com>
2021-01-14 10:43:05 +0100

    Add Docstrings for ArtNetAddress

--- cueglow-server/src/main/kotlin/org/cueglow/server/objects/ArtNetAddress.kt
+++ cueglow-server/src/main/kotlin/org/cueglow/server/objects/ArtNetAddress.kt
@@ -4,10 +4,21 @@ import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Result
 import com.github.michaelbull.result.Ok
 
+/**
+ * Represents an Art-Net v4 Port-Address.
+ *
+ * Can only be instantiated with [tryFrom].
+ *
+ * @property[value] A Short representing the 15-bit Port-Address.
+ */
 class ArtNetAddress private constructor(val value: Short) {
 
     companion object Factory {
-        // from Short
+        /**
+         * Instantiate ArtNetAddress from Short.
+         *
+         * @return Result of valid ArtNetAddress or Error for invalid input (i.e. negative)
+         */
         fun tryFrom(input: Short): Result<ArtNetAddress, InvalidArtNetAddress> {
             return if (input in 0..32_767) {
                 Ok(ArtNetAddress(input))
@@ -15,7 +26,11 @@ class ArtNetAddress private constructor(val value: Short) {
                 Err(InvalidArtNetAddress)
             }
         }
-        // from Int
+        /**
+         * Instantiate ArtNetAddress from Int.
+         *
+         * @return Result of valid ArtNetAddress or Error for invalid input (i.e. negative or too big)
+         */
         fun tryFrom(input: Int): Result<ArtNetAddress, InvalidArtNetAddress> {
             return if (input in 0..32_767) {
                 Ok(ArtNetAddress(input.toShort()))
@@ -23,7 +38,14 @@ class ArtNetAddress private constructor(val value: Short) {
                 Err(InvalidArtNetAddress)
             }
         }
-        // from Triple
+        /**
+         * Instantiate ArtNetAddress from Net, Sub-Net and Universe.
+         *
+         * Net must be a 7-bit number from 0 to 127, Sub-Net a 4-bit number from 0 to 15
+         * and Universe a 4-bit number from 0 to 15.
+         *
+         * @return Result of valid ArtNetAddress or Error for invalid input
+         */
         fun tryFrom(net: Int, subNet: Int, universe: Int): Result<ArtNetAddress, ArtNetAddressError> {
             return when {
                 net !in 0..127 -> Err(InvalidArtNetNet)
@@ -37,10 +59,18 @@ class ArtNetAddress private constructor(val value: Short) {
         }
     }
 
-
+    /**
+     * @return 7-bit Art-Net "Net" address as Int
+     */
     fun getNet() = (value.toInt() shr 8) and 0b111_1111
 
+    /**
+     * @return 4-bit Art-Net "Sub-Net" address as Int
+     */
     fun getSubNet() = (value.toInt() shr 4) and 0b1111
 
+    /**
+     * @return 4-bit Art-Net "Universe" address as Int
+     */
     fun getUniverse() = value.toInt() and 0b1111
 }
