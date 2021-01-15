5df641df4b0237a9b62a16cef81c1935e1892959
Johannes Fried <jcmf.schule@gmail.com>
2021-01-15 17:38:40 +0100

    Docstring and Automatic UUID for PatchFixture

---
+++ cueglow-server/src/main/kotlin/org/cueglow/server/patch/Patch.kt
@@ -0,0 +1,15 @@
+package org.cueglow.server.patch
+
+import java.util.*
+
+// TODO import GDTF class from gdtf branch
+class GDTF
+
+object Patch {
+    val patchFixtureList: HashMap<UUID, PatchFixture> = HashMap()
+    val fixtureTypeList: HashMap<UUID, GDTF> = HashMap()
+
+//    TODO
+//    setters that call stream update handler -> use Deletages.observable
+//    via singleton/static websocket stuff
+}
--- cueglow-server/src/main/kotlin/org/cueglow/server/patch/PatchFixture.kt
+++ cueglow-server/src/main/kotlin/org/cueglow/server/patch/PatchFixture.kt
@@ -4,29 +4,32 @@ import org.cueglow.server.objects.ArtNetAddress
 import org.cueglow.server.objects.DmxAddress
 import java.util.*
 
+/**
+ * A single Fixture in the Patch
+ *
+ * @property[fid] The **F**ixture **ID**, an Int arbitrarily assigned by the user (negative FIDs are still questionable)
+ * @property[name] The Fixture Name, assigned by the user
+ * @property[fixtureTypeId] The UUID of the Fixture Type as specified in the corresponding GDTF file
+ * @property[dmxMode] The DMX Mode, chosen by the user from the ones supplied in the GDTF file
+ * @property[universe] The Art-Net Port-Address of the Universe in which the Fixture operatres, assigned by the user
+ * @property[address] The DMX Address in the specified Universe, assigned by the user
+ * @property[uuid] The UUID of the Fixture, assigned by the Server upon creation
+ *
+ * The properties `universe` and `address` are nullable.
+ * If at least one of `universe` or `address` is null, no Art-Net output should be sent for this fixture.
+ */
 data class PatchFixture(
-    val uuid: UUID,
     var fid: Int,
     var name: String,
     val fixtureTypeId: UUID,
     val dmxMode: String,
-    var universe: ArtNetAddress,
-    var address: DmxAddress,
+    var universe: ArtNetAddress?,
+    var address: DmxAddress?,
 ) {
+    val uuid: UUID = UUID.randomUUID()
+
     override fun hashCode() = this.uuid.hashCode()
 }
-
-class Patch(
-    val patchFixtureList: HashMap<UUID, PatchFixture>,
-//    val fixtureTypeList: HashMap<UUID, GDTF>,
-) {
-//    TODO
-//    setters that call stream update handler
-//    via singleton/static websocket stuff
-}
-
-
-//FixtureTypeList: GDTF[]
 
 
 
