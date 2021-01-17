02051d3148bb06fb4cf32016a7aa86bf862d141f
Johannes Fried <jcmf.schule@gmail.com>
2021-01-18 00:08:54 +0100

    WIP Manually Modified XSD file to work properly

--- cueglow-server/src/main/kotlin/org/cueglow/server/gdtf/GdtfWrapper.kt
+++ cueglow-server/src/main/kotlin/org/cueglow/server/gdtf/GdtfWrapper.kt
@@ -5,14 +5,29 @@ import java.util.*
 
 /**
  * Immutable wrapper around GDTF
+ *
+ * Currently only provides the most basic properties
  */
 class GdtfWrapper(private val gdtf: GDTF) {
     val name = gdtf.fixtureType.name
     val manufacturer = gdtf.fixtureType.manufacturer
     val fixtureTypeId = UUID.fromString(gdtf.fixtureType.fixtureTypeID)
     val modes: List<DmxMode> = run {
-        gdtf.fixtureType.attributeDefinitionsOrWheelsOrPhysicalDescriptions.
-    }
+        gdtf.fixtureType.dmxModes.dmxMode.map {
+            // TODO
+            // calculating channel count is hard for GDTF
+            // https://gdtf-share.com/forum/index.php?/topic/340-getting-channel-count-from-a-gdtf-fixture/
+            // current approach does not work for multi-instance fixtures
+
+            val dmxChannels = it.dmxChannels?.dmxChannel ?: TODO("Err")
+
+            val channelCount = dmxChannels.map{ dmxChannel ->
+                        val offsetString = dmxChannel.offset ?: TODO("Err")
+                        offsetString.split(",").map { it.toInt() }.maxOf {it}
+                    }.maxOrNull() ?: TODO("Err")
+
+            DmxMode(it.name, channelCount)
+        }}
 }
 
 data class DmxMode(val name: String, val channelCount: Int)
