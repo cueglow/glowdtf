bb65491ad9880562be506a4a1d35a283abc1c466
Johannes Fried <jcmf.schule@gmail.com>
2021-03-19 21:45:29 +0100

    Clean Up With Restarting Server

--- cueglow-server/src/test/kotlin/org/cueglow/server/integration/ApiIntegrationTest.kt
+++ cueglow-server/src/test/kotlin/org/cueglow/server/integration/ApiIntegrationTest.kt
@@ -3,10 +3,9 @@ package org.cueglow.server.integration
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.FileDataPart
 import com.github.kittinunf.fuel.core.ResponseResultOf
+import com.github.michaelbull.result.Ok
 import com.github.michaelbull.result.unwrap
-import org.apache.logging.log4j.kotlin.Logging
 import org.awaitility.Awaitility
-import org.awaitility.Awaitility.await
 import org.awaitility.pollinterval.FibonacciPollInterval.fibonacci
 import org.cueglow.server.CueGlowServer
 import org.cueglow.server.gdtf.FixtureType
@@ -15,27 +14,23 @@ import org.cueglow.server.objects.ArtNetAddress
 import org.cueglow.server.objects.DmxAddress
 import org.cueglow.server.patch.Patch
 import org.cueglow.server.patch.PatchFixture
-import org.java_websocket.client.WebSocketClient
-import org.java_websocket.handshake.ServerHandshake
 import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
 import java.io.File
 import java.io.InputStream
 import java.net.URI
 import java.time.Duration
 import java.util.*
-import kotlin.collections.ArrayDeque
 
 /**
- * Tests the WebSocket and RESt API in one stateful sweep.
+ * Tests the WebSocket and REST API
  *
- * Provides the environment as well as ordering and listing the run tests.
- * However the individual tests are placed in other files as top-level functions.
+ * Provides setup/teardown of the server for each test and utility functions.
  *
- * To wait for the right response, we use Awaitility.
+ * Individual tests are written as free functions in other files and called in @Test-methods.
  *
- * When debugging the tests, I'd recommended to look at the whole standard output, not just the one from the failing
- * test. The individual tests tend to break each other.
+ * To wait for responses/state-changes we use Awaitility.
  */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(OrderAnnotation::class)
@@ -43,13 +38,6 @@ internal class ApiIntegrationTest {
     //-----------------------------------------------------
     // Initialization
     //-----------------------------------------------------
-    private val startTime = System.nanoTime()
-
-    fun printElapsed() {
-        val elapsed = (System.nanoTime() - startTime)/1e6
-        println("$elapsed ms elapsed")
-    }
-
     private lateinit var server: CueGlowServer
 
     private lateinit var patch: Patch
@@ -61,13 +49,17 @@ internal class ApiIntegrationTest {
         Awaitility.setDefaultTimeout(Duration.ofSeconds(2))
     }
 
-    private val exampleGdtfFileName = "Robe_Lighting@Robin_Esprite@20112020v1.7.gdtf"
-    private val inputStream: InputStream = javaClass.classLoader.getResourceAsStream(exampleGdtfFileName) ?:
-    throw Error("inputStream is Null")
-    private val parsedGdtf = parseGdtf(inputStream).unwrap()
-    private val exampleFixtureType = FixtureType(parsedGdtf)
+    //-----------------------------------------------------
+    // Helpers
+    //-----------------------------------------------------
 
-    val examplePatchFixture = PatchFixture(
+    private val exampleGdtfFileName = "Robe_Lighting@Robin_Esprite@20112020v1.7.gdtf"
+    private val exampleGdtfInputStream: InputStream =
+        javaClass.classLoader.getResourceAsStream(exampleGdtfFileName) ?: throw Error("inputStream is Null")
+    private val parsedExampleGdtf = parseGdtf(exampleGdtfInputStream).unwrap()
+    private val exampleFixtureType = FixtureType(parsedExampleGdtf)
+
+    private val examplePatchFixture = PatchFixture(
         UUID.randomUUID(),
         1,
         "exampleFixture",
@@ -76,6 +68,31 @@ internal class ApiIntegrationTest {
         ArtNetAddress.tryFrom(1).unwrap(),
         DmxAddress.tryFrom(1).unwrap(),
     )
+
+    private fun uploadGdtfFile(filename: String, partname: String = "file"): ResponseResultOf<String> {
+        val exampleGdtfFile =
+            File(javaClass.classLoader.getResource(filename)?.file ?: throw Error("can't get resource"))
+        return Fuel.upload("http://localhost:7000/api/fixturetype")
+            .add(
+                FileDataPart(
+                    exampleGdtfFile, name = partname, filename = filename
+                )
+            )
+            .responseString()
+    }
+
+    private fun setupExampleFixtureType() {
+        assertTrue(patch.addFixtureTypes(listOf(exampleFixtureType)) is Ok)
+    }
+
+    private fun setupExampleFixture() {
+        setupExampleFixtureType()
+        assert(patch.addFixtures(listOf(examplePatchFixture)) is Ok)
+    }
+
+    //-----------------------------------------------------
+    // Setup/Teardown for Each Test
+    //-----------------------------------------------------
 
     @BeforeEach
     fun setup() {
@@ -91,161 +108,94 @@ internal class ApiIntegrationTest {
         server.stop()
     }
 
-    //----------------------------
-    // Test Series 1
-    //----------------------------
+    //-----------------------------------------------------
+    // Individual Tests
+    //-----------------------------------------------------
+
+
+    // GDTF / Fixture Type Tests
 
     @Test
-    @Order(0)
     fun uploadGdtfFixtureType() = gdtfUploadTest(::uploadGdtfFile, patch)
 
-    // Add Fixture
+    @Test
+    fun invalidGdtfFileUpload() = invalidGdtfUploadTest(::uploadGdtfFile, patch)
 
     @Test
-    @Order(50)
-    fun addFixture() {
-        patch.addFixtureTypes(listOf(exampleFixtureType))
-        addFixtureTest(wsClient, patch, exampleFixtureType, examplePatchFixture)
-    }
+    fun noFilePartInGdtfUpload() = noFilePartInGdtfUploadErrors(::uploadGdtfFile)
 
     @Test
-    @Order(75)
-    fun addFixtureInvalidFixtureTypeId() {
-        patch.addFixtureTypes(listOf(exampleFixtureType))
-        addFixtureInvalidFixtureTypeIdTest(wsClient, patch)
-    }
+    fun gdtfWithoutDescriptionXml() = noDescriptionXmlUploadError(::uploadGdtfFile)
 
     @Test
-    @Order(87)
-    fun addFixtureInvalidDmxMode() {
-        patch.addFixtureTypes(listOf(exampleFixtureType))
-        addFixtureInvalidDmxModeTest(wsClient, patch)
-    }
-
-    // Update Fixture
-    @Test
-    @Order(90)
-    fun updateUnknownFixture() {
-        patch.addFixtureTypes(listOf(exampleFixtureType))
-        patch.addFixtures(listOf(examplePatchFixture))
-        updateUnknownFixtureTest(wsClient)
-    }
-
-    @Test
-    @Order(93)
-    fun updateAddress() {
-        patch.addFixtureTypes(listOf(exampleFixtureType))
-        patch.addFixtures(listOf(examplePatchFixture))
-        updateAddressTest(wsClient, patch)
-    }
-
-    @Test
-    @Order(96)
-    fun updateUniverse() {
-        patch.addFixtureTypes(listOf(exampleFixtureType))
-        patch.addFixtures(listOf(examplePatchFixture))
-        updateUniverseTest(wsClient, patch)
-    }
-
-    @Test
-    @Order(97)
-    fun updateNameAndFid() {
-        patch.addFixtureTypes(listOf(exampleFixtureType))
-        patch.addFixtures(listOf(examplePatchFixture))
-        updateNameAndFidTest(wsClient, patch)
-    }
-
-    @Test
-    @Order(98)
-    fun deleteInvalidFixture() {
-        patch.addFixtureTypes(listOf(exampleFixtureType))
-        patch.addFixtures(listOf(examplePatchFixture))
-        deleteInvalidFixtureTest(wsClient, patch)
-    }
-
-    @Test
-    @Order(99)
-    fun deleteFixture() {
-        patch.addFixtureTypes(listOf(exampleFixtureType))
-        patch.addFixtures(listOf(examplePatchFixture))
-        deleteFixtureTest(wsClient, patch)
-    }
-
-    @Test
-    @Order(100)
     fun deleteInvalidFixtureTypes() {
-        patch.addFixtureTypes(listOf(exampleFixtureType))
+        setupExampleFixture()
         deleteInvalidFixtureTypesTest(wsClient, patch)
     }
 
     @Test
-    @Order(150)
     fun deleteGdtfFixtureType() {
-        patch.addFixtureTypes(listOf(exampleFixtureType))
+        setupExampleFixture()
         gdtfDeleteTest(wsClient, patch)
     }
 
-    //-----------------------------------------------------
-    // Invalid GDTF Upload Tests (don't change state)
-    //-----------------------------------------------------
+    // Add Fixture Tests
 
     @Test
-    @Order(200)
-    fun invalidGdtfFileUpload() = invalidGdtfUploadTest(::uploadGdtfFile, patch)
+    fun addFixture() {
+        setupExampleFixtureType()
+        addFixtureTest(wsClient, patch, exampleFixtureType, examplePatchFixture)
+    }
 
     @Test
-    @Order(300)
-    fun noFilePartInGdtfUpload() = noFilePartInGdtfUploadErrors(::uploadGdtfFile)
+    fun addFixtureInvalidFixtureTypeId() {
+        setupExampleFixtureType()
+        addFixtureInvalidFixtureTypeIdTest(wsClient, patch)
+    }
 
     @Test
-    @Order(400)
-    fun gdtfWithoutDescriptionXml() = noDescriptionXmlUploadError(::uploadGdtfFile)
-
-    //-----------------------------------------------------
-    // Teardown
-    //-----------------------------------------------------
-
-    @AfterAll
-    fun shutdown() {
-        printElapsed()
+    fun addFixtureInvalidDmxMode() {
+        setupExampleFixtureType()
+        addFixtureInvalidDmxModeTest(wsClient, patch)
     }
 
-    private fun uploadGdtfFile(filename: String, partname: String = "file"): ResponseResultOf<String> {
-        val exampleGdtfFile= File(javaClass.classLoader.getResource(filename)?.file ?: throw Error("can't get resource"))
-        return Fuel.upload("http://localhost:7000/api/fixturetype")
-            .add(
-                FileDataPart(
-                    exampleGdtfFile, name=partname, filename=filename
-                )
-            )
-            .responseString()
-    }
-}
+    // Update Fixture Tests
 
-// Barebones WebSocket client for sending test messages
-class WsClient(uri: URI): WebSocketClient(uri), Logging {
-    private val receivedMessages = ArrayDeque<String>()
-
-    override fun onOpen(handshakedata: ServerHandshake?) {
-        logger.info("WsClient opened")
+    @Test
+    fun updateUnknownFixture() {
+        setupExampleFixture()
+        updateUnknownFixtureTest(wsClient)
     }
 
-    override fun onMessage(message: String?) {
-//        val pretty: String = (Parser.default().parse(StringBuilder(message)) as JsonObject).toJsonString(true)
-        logger.info("WsClient received $message")
-        receivedMessages.addLast(message ?: "")
+    @Test
+    fun updateAddress() {
+        setupExampleFixture()
+        updateAddressTest(wsClient, patch)
     }
 
-    override fun onClose(code: Int, reason: String?, remote: Boolean) {
-        logger.info("WsClient closed. Code $code. Reason: $reason")
+    @Test
+    fun updateUniverse() {
+        setupExampleFixture()
+        updateUniverseTest(wsClient, patch)
     }
 
-    override fun onError(ex: Exception?) {
-        logger.info("WsClient errored" + ex.toString())
+    @Test
+    fun updateNameAndFid() {
+        setupExampleFixture()
+        updateNameAndFidTest(wsClient, patch)
     }
 
-    fun receiveOneMessageBlocking(): String {
-        await().until { receivedMessages.isNotEmpty() }
-        return receivedMessages.removeFirst()
+    // Delete Fixture Tests
+
+    @Test
+    fun deleteInvalidFixture() {
+        setupExampleFixture()
+        deleteInvalidFixtureTest(wsClient, patch)
+    }
+
+    @Test
+    fun deleteFixture() {
+        setupExampleFixture()
+        deleteFixtureTest(wsClient, patch)
     }
 }
---
+++ cueglow-server/src/test/kotlin/org/cueglow/server/integration/WsClient.kt
@@ -0,0 +1,35 @@
+package org.cueglow.server.integration
+
+import org.apache.logging.log4j.kotlin.Logging
+import org.awaitility.Awaitility
+import org.java_websocket.client.WebSocketClient
+import org.java_websocket.handshake.ServerHandshake
+import java.net.URI
+
+/*** WebSocket client for Testing */
+class WsClient(uri: URI): WebSocketClient(uri), Logging {
+    private val receivedMessages = ArrayDeque<String>()
+
+    override fun onOpen(handshakedata: ServerHandshake?) {
+        logger.info("WsClient opened")
+    }
+
+    override fun onMessage(message: String?) {
+//        val pretty: String = (Parser.default().parse(StringBuilder(message)) as JsonObject).toJsonString(true)
+        logger.info("WsClient received $message")
+        receivedMessages.addLast(message ?: "")
+    }
+
+    override fun onClose(code: Int, reason: String?, remote: Boolean) {
+        logger.info("WsClient closed. Code $code. Reason: $reason")
+    }
+
+    override fun onError(ex: Exception?) {
+        logger.info("WsClient errored" + ex.toString())
+    }
+
+    fun receiveOneMessageBlocking(): String {
+        Awaitility.await().until { receivedMessages.isNotEmpty() }
+        return receivedMessages.removeFirst()
+    }
+}
