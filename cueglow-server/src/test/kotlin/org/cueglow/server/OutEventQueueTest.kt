584d1072c55181330a9fb568be7aa9ae920f9bba
Johannes Fried <jcmf.schule@gmail.com>
2021-04-16 17:24:44 +0200

    Make Queue Tests for AddFixtureTypes More Complex

--- cueglow-server/src/test/kotlin/org/cueglow/server/OutEventQueueTest.kt
+++ cueglow-server/src/test/kotlin/org/cueglow/server/OutEventQueueTest.kt
@@ -1,19 +1,27 @@
 package org.cueglow.server
 
+import com.github.michaelbull.result.Err
 import org.cueglow.server.objects.messages.GlowMessage
 import org.cueglow.server.test_utilities.ExampleFixtureType
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import java.util.concurrent.TimeUnit
 
 class OutEventQueueTest {
     @Test
-    fun addingFixtureTypeCreatesOutEventInQueue() {
+    fun addingFixtureTypeCreatesOutEvent() {
         val state = StateProvider()
         val exampleFixtureType = ExampleFixtureType.esprite
+
+        // add the exampleFixtureType twice
+        // first time should work and create an OutEvent
         state.patch.addFixtureTypes(listOf(exampleFixtureType))
+        // the second time should error and not create an OutEvent
+        assertTrue(state.patch.addFixtureTypes(listOf(exampleFixtureType)) is Err)
 
         val expected = GlowMessage.AddFixtureTypes(listOf(exampleFixtureType))
+
         val actual = (state.outEventQueue.poll(2, TimeUnit.SECONDS) ?:
             throw Error("Nothing in Queue until timeout")) as GlowMessage.AddFixtureTypes
 
@@ -21,5 +29,33 @@ class OutEventQueueTest {
         assertEquals(1, actual.data.size)
         assertEquals(expected.data[0], actual.data[0])
         assertEquals(expected.messageId, actual.messageId)
+
+        // when we added the fixture type the second time, it should not have created an OutEvent and so now the
+        // Queue should be empty
+        assertEquals(null, state.outEventQueue.peek())
+    }
+
+    @Test
+    fun addingFixtureTypeTwiceOnlyReportsItOnce() {
+        val state = StateProvider()
+        val exampleFixtureType = ExampleFixtureType.esprite
+
+        // add the exampleFixtureType twice
+        // first time should work
+        // the second time should error
+        assertTrue(state.patch.addFixtureTypes(listOf(exampleFixtureType, exampleFixtureType)) is Err)
+
+        val expected = GlowMessage.AddFixtureTypes(listOf(exampleFixtureType))
+
+        val actual = (state.outEventQueue.poll(2, TimeUnit.SECONDS) ?:
+            throw Error("Nothing in Queue until timeout")) as GlowMessage.AddFixtureTypes
+
+        assertEquals(expected.event, actual.event)
+        assertEquals(1, actual.data.size)
+        assertEquals(expected.data[0], actual.data[0])
+        assertEquals(expected.messageId, actual.messageId)
+
+        // now the Queue should be empty
+        assertEquals(null, state.outEventQueue.peek())
     }
 }
