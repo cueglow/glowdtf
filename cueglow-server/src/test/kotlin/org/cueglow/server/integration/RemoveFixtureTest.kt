a715768913b2f69459a5daff7d0d69cc3026acc2
Johannes Fried <jcmf.schule@gmail.com>
2021-04-19 17:54:23 +0200

    Move Remove Fixture Integration Tests to Specific Class

--- cueglow-server/src/test/kotlin/org/cueglow/server/integration/RemoveFixtureTest.kt
+++ cueglow-server/src/test/kotlin/org/cueglow/server/integration/RemoveFixtureTest.kt
@@ -5,51 +5,60 @@ import org.cueglow.server.json.fromJsonString
 import org.cueglow.server.objects.messages.GlowEvent
 import org.cueglow.server.objects.messages.GlowMessage
 import org.cueglow.server.patch.Patch
+import org.cueglow.server.test_utilities.ClientAndServerTest
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 import java.util.*
 
+class RemoveFixtureIntegrationTest: ClientAndServerTest() {
+    @Test
+    fun removeInvalidFixture() {
+        setupExampleFixture()
 
-fun removeInvalidFixtureTest(wsClient: WsClient, patch: Patch) {
-    val uuid1 = UUID.fromString("753eca6f-f5a8-4e0d-8907-16074534e08f")
-    val uuid2 = UUID.fromString("9cf0450d-cef1-47e6-b467-631c4cfe45e7")
+        val uuid1 = UUID.fromString("753eca6f-f5a8-4e0d-8907-16074534e08f")
+        val uuid2 = UUID.fromString("9cf0450d-cef1-47e6-b467-631c4cfe45e7")
 
-    val jsonToSend =
-        """{
+        val jsonToSend =
+            """{
             "event": "removeFixtures",
             "data": ["$uuid1", "$uuid2"],
             "messageId": 729
         }""".trimIndent()
 
-    wsClient.send(jsonToSend)
+        wsClient.send(jsonToSend)
 
-    val msg1 = GlowMessage.fromJsonString(wsClient.receiveOneMessageBlocking())
-    val msg2 = GlowMessage.fromJsonString(wsClient.receiveOneMessageBlocking())
+        val msg1 = GlowMessage.fromJsonString(wsClient.receiveOneMessageBlocking())
+        val msg2 = GlowMessage.fromJsonString(wsClient.receiveOneMessageBlocking())
 
-    // still one fixture in patch
-    Assertions.assertEquals(1, patch.getFixtures().size)
+        // still one fixture in patch
+        Assertions.assertEquals(1, patch.getFixtures().size)
 
-    Assertions.assertEquals(GlowEvent.ERROR, msg1.event)
-    Assertions.assertEquals("UnknownFixtureUuidError", (msg1 as GlowMessage.Error).data.name)
-    Assertions.assertTrue(msg1.data.description.contains(uuid1.toString()))
+        Assertions.assertEquals(GlowEvent.ERROR, msg1.event)
+        Assertions.assertEquals("UnknownFixtureUuidError", (msg1 as GlowMessage.Error).data.name)
+        Assertions.assertTrue(msg1.data.description.contains(uuid1.toString()))
 
-    Assertions.assertEquals(GlowEvent.ERROR, msg2.event)
-    Assertions.assertEquals("UnknownFixtureUuidError", (msg2 as GlowMessage.Error).data.name)
-    Assertions.assertTrue(msg2.data.description.contains(uuid2.toString()))
-}
+        Assertions.assertEquals(GlowEvent.ERROR, msg2.event)
+        Assertions.assertEquals("UnknownFixtureUuidError", (msg2 as GlowMessage.Error).data.name)
+        Assertions.assertTrue(msg2.data.description.contains(uuid2.toString()))
+    }
 
-fun removeFixtureTest(wsClient: WsClient, patch: Patch) {
-    val uuidToModify = patch.getFixtures().keys.first()
+    @Test
+    fun removeFixture() {
+        setupExampleFixture()
 
-    val jsonToSend =
-        """{
+        val uuidToModify = patch.getFixtures().keys.first()
+
+        val jsonToSend =
+            """{
             "event": "removeFixtures",
             "data": ["$uuidToModify"],
             "messageId": 730
         }""".trimIndent()
 
-    wsClient.send(jsonToSend)
+        wsClient.send(jsonToSend)
 
-    Awaitility.await().untilAsserted {
-        Assertions.assertEquals(0, patch.getFixtures().size)
+        Awaitility.await().untilAsserted {
+            Assertions.assertEquals(0, patch.getFixtures().size)
+        }
     }
 }
