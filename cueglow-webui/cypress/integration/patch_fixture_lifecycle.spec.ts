42f654852b686c5606bb4c5807629bb77eabb12c
Johannes Fried <jcmf.schule@gmail.com>
2021-05-30 21:32:09 +0200

    Refactor Cypress Tests
    - Change all files to TypeScript
    - Refactor clearFixtureTypes to custom command
    - Add dataCy command example from website

--- cueglow-webui/cypress/integration/patch_fixture_lifecycle.spec.ts
+++ cueglow-webui/cypress/integration/patch_fixture_lifecycle.spec.ts
@@ -1,38 +1,6 @@
 describe("Patch Fixture Lifecycle", () => {
     beforeEach(() => {
-        // setup: remove all patched fixture types
-        const urlWithoutProtocol = Cypress.config().baseUrl.replace(/(^\w+:|^)\/\//, '');
-        const ws = new WebSocket(`ws://${urlWithoutProtocol}/ws`)
-        ws.onopen = () => {
-            ws.send(JSON.stringify({
-                event: "subscribe",
-                data: "patch",
-            }))
-        }
-        // this is how to await a Cypress.Promise (https://docs.cypress.io/api/utilities/promise#Waiting-for-Promises)
-        cy.wrap(null, {log: false}).then(() => {
-            return new Cypress.Promise<void>((resolve, reject) => {
-                ws.onmessage = (event) => {
-                    const parsed = JSON.parse(event.data)
-                    const uuids = parsed.data.fixtureTypes.map((ft) => {
-                        return ft.fixtureTypeId
-                    })
-                    if (!(uuids.length === 0)) {
-                        ws.send(JSON.stringify({
-                            event: "removeFixtureTypes",
-                            data: uuids,
-                            messageId: 827
-                        }))
-                        const debugNames = parsed.data.fixtureTypes.map((ft) => {
-                            return `${ft.manufacturer} ${ft.name} (${ft.fixtureTypeId})`
-                        })
-                        cy.log("Removed Fixture Types before test: " + debugNames)
-                    }
-                    ws.close()
-                    resolve()
-                }
-            })
-        })
+        cy.clearFixtureTypes()
     })
 
     it("adds Fixture Type, adds Fixtures, removes Fixtures and removes Fixture Type", () => {
@@ -41,7 +9,7 @@ describe("Patch Fixture Lifecycle", () => {
         // Add GDTF
         // The user would click on the "Add GDTF" button, but Cypress can't easily
         // interact with the file picker, so we directly attach a GDTF file to the input
-        cy.get("[data-cy='gdtf_hidden_input']")
+        cy.dataCy("gdtf_hidden_input")
             .attachFile({
                 filePath: "Generic@Tungsten_8Bit@Update_to_GDTF_1.1.gdtf",
                 encoding: "binary"
@@ -75,14 +43,14 @@ describe("Patch Fixture Lifecycle", () => {
         cy.contains(`${testFixtureName} 2`).click()
 
         // Remove One Fixture
-        cy.get("[data-cy='remove_selected_fixture_button']").click()
+        cy.dataCy("remove_selected_fixture_button").click()
         cy.contains(`${testFixtureName} 1`)
         cy.contains(`${testFixtureName} 2`).should("not.exist")
 
         // Remove GDTF
         cy.visit("/patch/fixtureTypes")
         cy.contains(fixtureTypeName).click()
-        cy.get("[data-cy='remove_selected_fixture_type_button']").click()
+        cy.dataCy("remove_selected_fixture_type_button").click()
 
         // check GDTF is gone
         cy.contains(fixtureTypeName).should("not.exist")
