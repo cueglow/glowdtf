261c5335ff2231e82b1b1e5c5be5896c32035316
Johannes Fried <jcmf.schule@gmail.com>
2021-12-23 02:44:13 +0100

    Update Cypress

--- cueglow-webui/src/App/App.tsx
+++ cueglow-webui/src/App/App.tsx
@@ -2,7 +2,6 @@ import { HotkeysProvider } from '@blueprintjs/core';
 import { Router } from '@reach/router';
 import React from 'react';
 import { PatchDataProvider } from 'src/ConnectionProvider/PatchDataProvider';
-import { RigStateProvider } from 'src/ConnectionProvider/RigStateProvider';
 import { PatchWindow } from '../PatchWindow/PatchWindow';
 import { MainWindow } from './MainWindow';
 
